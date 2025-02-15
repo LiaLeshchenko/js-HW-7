e663a766a068d7326ed0fe5129446220ee1f8f04
LiaLeshchenko <156839096+LiaLeshchenko@users.noreply.github.com>
2025-02-15 17:29:33 +0200

    add style

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// Створити масив з трьох чисел.
+// Змінити значення другого елемента масиву на 10.
+
+// const number = [1, 2, 3];
+// number[1] = 10;
+// console.log(number);
+
+
+// Створити масив із трьох рядків.Додати до масиву ще одну рядків.
+
+// const fruit = ["Apple", "Pear", "Coconat"];
+// fruit[3] = "Pineapple";
+// console.log(`its my favourite fruits: ${fruit}`);
+
+
+// Створити скрипт який поверне суму всіх чисел в масиві.
+
+// const numbers = [5, 1, 2];
+// let total = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+// total += numbers[i];}
+// console.log(total);
+
+
+// Створити масив з 5-ти чисел.// Вивести на екран всі елементи масиву за допомогою циклу for.
+
+// const numbers = [0, 1, 2, 3, 4];
+// for (let i = 0; i <= numbers.length - 1; i += 1) {
+// console.log(numbers[i]);
+// }
+
+
+
+// Створити масив із 5-ти рядків.// Вивести на екран кожен рядокз масиву, який містить більше 5 - ти символів.
+
+// const rooms = [
+// "Bathroom",
+// "living room",
+// "Kitchen",
+// "Garden",
+// "Hallway",
+// ];
+// for (let i = 0; i < rooms.length; i += 1) {
+// if (rooms[i].length >= 5) {
+// console.log(`Rooms in the hous: ${rooms[i]}`);
+// } else {
+// console.log(`There are other rooms in the house`);
+// }
+// }
+
+
+// Створити масив з 10-ти чисел.Знайти та вивести на екран максимальне значення з масиву.
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 9, 8, 9,];
+// let max = numbers[0];
+// for (let i = 0; i < numbers.length; i += 1) {
+// if (numbers[i] > max) {
+// max = numbers[i];
+// }
+// }
+// console.log(max);
+
+
+
+// Створити масив з 10-ти чисел.// Знайти всі парні числа в масиві та вивести їх на екран.
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+// for (let i = 0; i < numbers.length; i += 1) {
+// if (numbers[i] % 2 === 0) {
+// console.log(numbers[i]);
+// }
+// }
